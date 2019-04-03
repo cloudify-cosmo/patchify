75f5f3fc65577c1f8beeaa5ca70a5c92671c6437
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-04-03 20:05:52 +0200

    Update the 4.5.5 dont-break-amqp-postgres patch

--- patch_files/patches/4_5_5_dont_break_amqp_postgres_amqp-postgres_amqp_postgres_postgres_publisher.py
+++ patch_files/patches/4_5_5_dont_break_amqp_postgres_amqp-postgres_amqp_postgres_postgres_publisher.py
@@ -1,8 +1,16 @@
 diff --git a/amqp-postgres/amqp_postgres/postgres_publisher.py b/amqp-postgres/amqp_postgres/postgres_publisher.py
-index 416eaf1..7ac13eb 100644
+index 416eaf1..a3e1a8e 100644
 --- a/amqp-postgres/amqp_postgres/postgres_publisher.py
 +++ b/amqp-postgres/amqp_postgres/postgres_publisher.py
-@@ -189,9 +189,9 @@ class DBLogEventPublisher(object):
+@@ -21,6 +21,7 @@ from time import time
+ from threading import Thread, Lock
+ 
+ import psycopg2
++import psycopg2.errorcodes
+ from psycopg2.extras import execute_values, DictCursor
+ from collections import OrderedDict
+ from cloudify.constants import EVENTS_EXCHANGE_NAME, LOGS_EXCHANGE_NAME
+@@ -189,9 +190,9 @@ class DBLogEventPublisher(object):
                      self._store(conn, items)
                  except psycopg2.OperationalError as e:
                      self.on_db_connection_error(e)
@@ -15,7 +23,7 @@ index 416eaf1..7ac13eb 100644
                      conn.rollback()
                      # in case the integrityError was caused by stale cache,
                      # clean it entirely before trying to insert without
-@@ -274,6 +274,18 @@ class DBLogEventPublisher(object):
+@@ -274,6 +275,18 @@ class DBLogEventPublisher(object):
              except psycopg2.IntegrityError:
                  logger.debug('Error storing %s: %s', exchange, item)
                  conn.rollback()
