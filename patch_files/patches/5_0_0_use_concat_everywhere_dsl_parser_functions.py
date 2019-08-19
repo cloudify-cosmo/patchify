7fa63b529dc5d55660570ca315c4557897e64413
tyacbovi <3802463+tyacbovi@users.noreply.github.com>
2019-08-19 11:19:27 +0300

    adding 5_0_0_use_concat_everywhere

---
+++ patch_files/patches/5_0_0_use_concat_everywhere_dsl_parser_functions.py
@@ -0,0 +1,17 @@
+diff --git a/dsl_parser/functions.py b/dsl_parser/functions.py
+index 2345532c..1b91f5d8 100644
+--- a/dsl_parser/functions.py
++++ b/dsl_parser/functions.py
+@@ -682,12 +682,6 @@ class Concat(Function):
+                 'Using {0} requires using dsl version 1_1 or '
+                 'greater, but found: {1} in {2}.'
+                 .format(self.name, plan.version, self.path))
+-        if self.scope not in [scan.NODE_TEMPLATE_SCOPE,
+-                              scan.NODE_TEMPLATE_RELATIONSHIP_SCOPE,
+-                              scan.OUTPUTS_SCOPE]:
+-            raise ValueError('{0} cannot be used in {1}.'
+-                             .format(self.name,
+-                                     self.path))
+ 
+     def evaluate(self, plan):
+         for joined_value in self.joined:
